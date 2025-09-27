fbe9fd244397b044f3231789f8086bdb15a20bd9
Shahriar-Labib <labibshahriar028@gmail.com>
2025-09-27 21:15:01 +0600

    frontend

---
+++ src/main/java/com/OnlineCart/OnlineCart/OnlineCartApplication.java
@@ -0,0 +1,13 @@
+package com.OnlineCart.OnlineCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.OnlineCart")
+public class OnlineCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OnlineCart/OnlineCart/OnlineCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineCart.OnlineCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
